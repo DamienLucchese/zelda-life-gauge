f867ef8d2c3e24d01a5caa1f06add79d718e9ead
Damien Lucchese <dlucchese@shopkeep.com>
2020-05-08 10:11:14 -0400

    still working to resolve error

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 //import  from "./js/components/ ";
+
+import './index.scss'
+import React from 'react'
+
+const App = () => (
+  <div className="testBox">
+    <p className="testBox--text">Congrats! This Worked.</p>
+  </div>
+);
+
+ReactDOM.render(<App />, document.querySelector('#app'));
